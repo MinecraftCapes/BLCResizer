340c4f6be6732607faf0801a69462d88a729ca22
James Harrison <james@fasttortoise.co.uk>
2021-12-12 14:16:34 +0000

    📜 Fix redownload each time

--- index.php
+++ index.php
@@ -1,26 +1,18 @@
 <?php
 
     //CHANGE THIS
-    // $ASSET_URL = "https://client-jars.badlion.net/common-assets/PRODUCTION3/v3.4.1-19fbed2-PRODUCTION3-assets.json";
-    $ASSET_URL = "https://client-jars.badlion.net/common-assets/INSIDER3/v3.4.1-0a703e6-INSIDER3-assets.json";
+    $ASSET_URL = "https://client-jars.badlion.net/common-assets/PRODUCTION3/v3.6.1-5fb8ab6-PRODUCTION3-assets.json";
+    // $ASSET_URL = "https://client-jars.badlion.net/common-assets/INSIDER3/v3.4.1-0a703e6-INSIDER3-assets.json";
 
     //Don't change me
-    // $base_url = "https://client-jars.badlion.net/common-assets/PRODUCTION3/assets";
-    $base_url = "https://client-jars.badlion.net/common-assets/INSIDER3/assets";
+    $base_url = "https://client-jars.badlion.net/common-assets/PRODUCTION3/assets";
+    // $base_url = "https://client-jars.badlion.net/common-assets/INSIDER3/assets";
 
     // Include composer autoload
     require 'vendor/autoload.php';
 
     // Import classes
     use Intervention\Image\ImageManagerStatic as Image;
-    use Grinderspro\DirectoryManipulator\DirectoryManipulator;
-
-    /**
-     * Delete the out dir and copy in to new out (avoid subfolder complications)
-     */
-    echo "Emptying directory!\n";
-    (new DirectoryManipulator())->location('./images')->clear();
-    echo "Done!\n\n";
 
     /**
      * Download and convert the capes
@@ -28,12 +20,26 @@
     echo "Downloading capes...\n";
     $blcAssets = json_decode(file_get_contents($ASSET_URL));
     foreach($blcAssets as $key => $asset) {
-        if(strpos($key, "assets/minecraft/blc/textures/cosmetics/cloak") !== false) {
+        if(str_contains($key, "assets/minecraft/blc/textures/cosmetics/cloak")) {
             $fileName = basename($key);
             $fileLocation = "images/" . str_replace("assets/minecraft/blc/textures/cosmetics/cloak/", "", str_replace($fileName, "", $key));
+
+            //We only really want png files
+            if(pathinfo($fileName)['extension'] != "png") {
+                continue;
+            }
+
+            //Lets not redownload the same stuff
+            if(file_exists("$fileLocation/$fileName")) {
+                continue;
+            }
+
+            //Lets make the folder for the cape
             if(!file_exists($fileLocation)) {
                 mkdir($fileLocation);
             }
+
+            //Lets try download and convert it
             try {
                 Image::make("$base_url/$asset->name")->crop(704, 544, 0, 0)->resizeCanvas(2048, 1024, 'top-left')->save("$fileLocation/$fileName");
                 echo "Completed Conversion - $fileName\n";
@@ -44,4 +50,4 @@
             }
         }
     }
-    echo "\n\nFinished!";
+    echo "\n\nFinished!\n";
