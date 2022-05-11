f3990205d3787359e4b6df4a3bfa5092f294a1c3
james090500 <james@fasttortoise.co.uk>
2022-05-11 19:08:06 +0100

    😓 Hopefully fixes most resize bugs

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const axios = require('axios');
 const cliProgress = require('cli-progress');
 const fs = require('fs');
 const path = require('path');
+const { exit } = require("process");
 const sharp = require('sharp');
 
 //CHANGE THIS
@@ -32,69 +33,71 @@ async function start() {
     })
 
     logger.info("Requesting assets...")
-    axios.get(ASSET_URL).then((response) => {
+    let assetData = await axios.get(ASSET_URL).then((response) => {
         logger.info("Obtained assets.");
-
-        //Filted used items
-        logger.info("Filtering api response...")
-        let allCapes = Object.entries(response.data).filter(([key, _value]) => key.includes("assets/minecraft/blc/textures/cosmetics/cloak"))
-        logger.info("Filtering complete!")
-
-        //Start a progress bar
-        logger.info("Downloading capes, this can take a long time...")
-        progressBar.start(allCapes.length, 0);
-
-        //Loop through capes
-        allCapes.forEach(([key, asset]) => {
-            let fileName = path.basename(key);
-            let fileLocation = `images/${key.replace("assets/minecraft/blc/textures/cosmetics/cloak/", "").replace(fileName, "")}`;
-
-            //We only really want png files
-            if(!fileName.endsWith(".png")) {
-                return;
-            }
-
-            //Lets not redownload the same stuff
-            if(fs.existsSync(`${fileLocation}${fileName}`)) {
-                return;
-            }
-
-            //Lets make the folder for the cape
-            if(!fs.existsSync(fileLocation)) {
-                fs.mkdirSync(fileLocation, { recursive: true });
-            }
-
-            //Download and convert images
-            let textureUrl = `${base_url}${asset.name}`
-            axios.get(textureUrl, { responseType: 'arraybuffer' }).then((res) => {
-                let image = sharp(res.data)
-                image.metadata().then((metadata) => {
-                    return image.extract({ left: 0, top: 0, width: metadata.width, height: Math.round(metadata.width * (17 / 22)) })
-                        .resize(352, 272, {
-                            fith: 'fill',
-                            position: 'left top'
-                        })
-                    .then((image) => {
-                        image.extend({ top: 0, left: 0, bottom: 1024 - metadata.height, right: 2048 - metadata.width })
-                        .toFile(`${fileLocation}${fileName}`)
-                        .catch(err => {
-                            logger.error(`${err} - ${textureUrl}`);
-                        })
-                        .finally(() => {
-                            progressBar.increment();
-                        })
-                    })
-                });
-            }).catch((error) => {
-                progressBar.increment();
-                logger.error(`Failed to download texture - ${textureUrl}`)
-                logger.error(error.message);
-            });
-        })
+        return response.data;
     }).catch((error) => {
         progressBar.stop();
         logger.error("Obtaining assets failed.")
         logger.error(error.message);
+    })
+
+    //Lets die if no data
+    if(assetData == null) return;
+
+    //Filted used items
+    logger.info("Filtering api response...")
+    let allCapes = Object.entries(assetData).filter(([key, _value]) => key.includes("assets/minecraft/blc/textures/cosmetics/cloak"))
+    logger.info("Filtering complete!")
+
+    //Start a progress bar
+    logger.info("Downloading capes, this can take a long time...")
+    progressBar.start(allCapes.length, 0);
+
+    //Loop through capes
+    allCapes.forEach(([key, asset]) => {
+        let fileName = path.basename(key);
+        let fileLocation = `images/${key.replace("assets/minecraft/blc/textures/cosmetics/cloak/", "").replace(fileName, "")}`;
+
+        //We only really want png files
+        if(!fileName.endsWith(".png")) {
+            return;
+        }
+
+        //Lets not redownload the same stuff
+        if(fs.existsSync(`${fileLocation}${fileName}`)) {
+            return;
+        }
+
+        //Lets make the folder for the cape
+        if(!fs.existsSync(fileLocation)) {
+            fs.mkdirSync(fileLocation, { recursive: true });
+        }
+
+        //Download and convert images
+        let textureUrl = `${base_url}${asset.name}`
+        axios.get(textureUrl, { responseType: 'arraybuffer' }).then((res) => {
+            let image = sharp(res.data)
+            image.metadata().then((metadata) => {
+                image.extract({ left: 0, top: 0, width: metadata.width, height: Math.round(metadata.width * (17 / 22)) })
+                .resize(352, 272, {
+                    fith: 'fill',
+                    position: 'left top'
+                })
+                .extend({ top: 0, left: 0, bottom: 512 - 272, right: 1024 - 352 })
+                .toFile(`${fileLocation}${fileName}`)
+                .catch(err => {
+                    logger.error(`${err} - ${textureUrl}`);
+                })
+                .finally(() => {
+                    progressBar.increment();
+                })
+            })
+        }).catch((error) => {
+            progressBar.increment();
+            logger.error(`Failed to download texture - ${textureUrl}`)
+            logger.error(error.message);
+        });
     })
 }
 
